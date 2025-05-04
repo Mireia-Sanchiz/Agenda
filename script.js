f6f04aa2a96ded95a739a15d95b77602a170571c
Mireia-Sanchiz <sanchiz.mireia@gmail.com>
2025-05-05 00:13:27 +0200

    Control d'errors per camps buits al formulari

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 // script.js
 console.log("Agenda Web");
+ocument.querySelector("form").addEventListener("submit", e => {
+e.preventDefault();
+const input = document.querySelector("#nom");
+if (input.value.trim() === "") {
+alert("El camp no pot estar buit");
+return;
+}
+const contactes = ["Jose", "Saida", "Ruben"];
+const llista = document.createElement('ul');
+contactes.forEach(nom => {
+const li = document.createElement('li');
+li.textContent = nom;
+llista.appendChild(li);
+});
+document.body.appendChild(llista);
+});
+
